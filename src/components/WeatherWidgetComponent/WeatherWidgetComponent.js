52831f81a8b3ede1776d93b6d3857dab2fdbfee3
Caetan <caetan.tojeiro.carpente@cern.ch>
2023-08-08 13:38:09 +0200

    Live coding

--- src/components/WeatherWidgetComponent/WeatherWidgetComponent.js
+++ src/components/WeatherWidgetComponent/WeatherWidgetComponent.js
@@ -12,29 +12,47 @@ const units = "metric";
 const locationApi = (place) => `http://api.openweathermap.org/geo/1.0/direct?q=${place}&limit=1&appid=${api_key}`;
 const watherApi = (lat, lon) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=${units}`;
 
+
+
+const fetchData = async (city) => {
+  // try {
+  const loc = await (await fetch(locationApi(city))).json();
+  if (!loc.length) {
+    console.error("City not found")
+  } else {
+    const wather = await (await fetch(watherApi(loc[0].lat, loc[0].lon))).json();
+    return wather
+  }
+  // }
+  // catch (error) {
+  //   setError(error)
+  // }
+}
+
 export const WeatherWidgetComponent = ({city}) => {
+  const cities = ["Almería", "Granada", "Málaga", "Jaén", "Córdoba", "Sevilla", "Huelva", "Cádiz"]
   const [weatherData, setWeatherData] = useState();
   const [error, setError] = useState();
+  const [avgtmp, setAvgTemp] = useState();
+  let avgTemp = 0
 
   useEffect(() => {
-    setError()
-    setWeatherData()
-    const fetchData = async () => {
-      try {
-        const loc = await (await fetch(locationApi(city || "Almeria"))).json();
-        if (!loc.length) {
-          setError("City not found")
-        } else {
-          const wather = await (await fetch(watherApi(loc[0].lat, loc[0].lon))).json();
-          setWeatherData(wather)
-        }
-      }
-      catch (error) {
-        setError(error)
-      }
-    }
-    fetchData();
-  }, [city])
+    const tmpWeather = []
+    async function fetchData2() {
+    // setError()
+    // setWeatherData()
+    cities.forEach(async (city) => {
+      const data = await fetchData(city)
+      tmpWeather.push({"city": city, "data": data});
+      console.log(data.main.temp, avgTemp)
+      avgTemp = avgTemp + data.main.temp
+    })
+  }
+  fetchData2();
+  setWeatherData(tmpWeather)
+  setAvgTemp(avgTemp / 8)
+  }, [])
+  console.log(avgtmp)
 
   if (error) {
     return (
@@ -44,42 +62,49 @@ export const WeatherWidgetComponent = ({city}) => {
 
   return (
     <>
-      <Card style={{textAlign: "center"}}>
-        {!weatherData ? (
+    Andalusia AVG Temperature = {avgTemp}
+     {/* {!weatherData ? (
           <Dimmer active inverted>
             <Loader content="Loading..." />
           </Dimmer>
-        )
-          : (
-            <>
-              <Card.Content>
-                <Card.Header>{weatherData.name} ({weatherData.sys.country})</Card.Header>
-                <Image size="small" src={`https://openweathermap.org/img/wn/${weatherData?.weather[0].icon}@2x.png`} />
-                <Card.Description>
-                  <p>
-                    <img
-                      src={Temperature}
-                      alt="Temperature Icon"
-                      style={tmpStyle}
-                    />
-                    Temperature: <b>{weatherData.main.temp} ºC</b>
-                  </p>
-                  <p>
-                    <img
-                      src={Feelslike}
-                      alt="Feels Like Icon"
-                      style={tmpStyle}
-                    />
-                    Feels like: <b>{weatherData.main.feels_like} ºC</b>
-                  </p>
-                </Card.Description>
-              </Card.Content>
-              {/* <Card.Content extra>
+          )
+          : ( */}
+    <Card.Group>
+      {weatherData?.map(({city, data: weatherData}) => {
+        return (
+        <Card style={{textAlign: "center"}} key={city}>
+         
+            <Card.Content>
+              <Card.Header>{weatherData.name} ({weatherData.sys.country})</Card.Header>
+              <Image size="small" src={`https://openweathermap.org/img/wn/${weatherData?.weather[0].icon}@2x.png`} />
+              <Card.Description>
+                <p>
+                  <img
+                    src={Temperature}
+                    alt="Temperature Icon"
+                    style={tmpStyle}
+                  />
+                  Temperature: <b>{weatherData.main.temp} ºC</b>
+                </p>
+                <p>
+                  <img
+                    src={Feelslike}
+                    alt="Feels Like Icon"
+                    style={tmpStyle}
+                  />
+                  Feels like: <b>{weatherData.main.feels_like} ºC</b>
+                </p>
+              </Card.Description>
+            </Card.Content>
+            {/* <Card.Content extra>
                 Last page update: {Date().toLocaleString()}
               </Card.Content> */}
-            </>
-          )}
-      </Card>
+          {/* ) */}
+        </Card>
+
+      )})}
+    </Card.Group>
+          {/* )} */}
     </>
   )
 }
